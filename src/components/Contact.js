4010a9963de2be2dfe34346e6b59750f354a1880
Krenar <krenarmuzaqi@gmail.com>
2020-05-19 19:05:06 +0200

    Context API added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import { Consumer } from "../context";
+
 class Contact extends Component {
   state = {
     showContactInfo: false,
@@ -11,42 +13,49 @@ class Contact extends Component {
     });
   };
 
-  onDeleteClick = () => {
-    this.props.deleteClickHandler();
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
   };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
-      <div className="card card-body mb-3">
-        <h4>
-          {name}{" "}
-          <i
-            onClick={this.onShowClick}
-            style={{ cursor: "pointer" }}
-            className="fas fa-sort-down"
-          ></i>
-          <i
-            onClick={this.onDeleteClick}
-            style={{ cursor: "pointer", float: "right", color: "red" }}
-            className="fas fa-times"
-          ></i>
-        </h4>
-        {showContactInfo ? (
-          <ul className="list-group">
-            <li className="list-group-item">Email: {email}</li>
-            <li className="list-group-item">Phone: {phone}</li>
-          </ul>
-        ) : null}
-      </div>
+      <Consumer>
+        {(value) => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{" "}
+                <i
+                  onClick={this.onShowClick}
+                  style={{ cursor: "pointer" }}
+                  className="fas fa-sort-down"
+                ></i>
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                  className="fas fa-times"
+                ></i>
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
 Contact.propTypes = {
   contact: PropTypes.object.isRequired,
-  deleteClickHandler: PropTypes.func.isRequired,
 };
+
 export default Contact;
